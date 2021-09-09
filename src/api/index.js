cb072ad08a93e2e339a29176a054c4928720a773
Sim Brody <sim@simbrody.co.uk>
2021-09-09 10:15:44 +0100

    test complete

--- src/api/index.js
+++ src/api/index.js
@@ -8,5 +8,37 @@ import { request } from './helpers';
  */
 // TODO: All API related logic should be made inside this function.
 export default async function getData() {
-  return [];
+  // api endpoints
+  const mainEndpoint = '/api/vehicles.json';
+  // const detailEndpoint = '/api/vehicle_fpace.json';
+  // const nopriceEndpoint = '/api/vehicle_xf.json';
+
+  // get data from main endpoint
+  const response = await fetch(mainEndpoint);
+  const data = await response.json();
+  console.log(data);
+
+  // map the endpoint array and return the ID of each vehicle
+  const vehicleIDs = data.map((vehicle) => {
+    return vehicle.id;
+  });
+  console.log(vehicleIDs);
+
+  // got to filter it first
+  // map the ID array and for each make a call to the vehicle detail api
+  const vehicleDetails = await Promise.all(vehicleIDs.map(async (id) => {
+    const detailsArray = await fetch(`/api/vehicle_${id}.json`)
+      .then((res) => res.json())
+      .catch(() => null);
+    return detailsArray;
+  }));
+  console.log(vehicleDetails);
+
+  // // filter vehicle details and remove responses returning error and those without a price
+  const filteredDetails = vehicleDetails.filter((res) => {
+    return res !== null && res.price !== '' && res.price;
+  });
+  console.log(filteredDetails);
+
+  return [data, filteredDetails];
 }
--- src/components/VehicleList/index.js
+++ src/components/VehicleList/index.js
@@ -1,40 +1,69 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import useData from './useData';
 import './style.scss';
 
 export default function VehicleList() {
+  // get screen size so correct image can be selected
+  const windowWidth = window.innerWidth;
+  const [currentWidth, setCurrentWidth] = useState(windowWidth);
+
+  useEffect(() => {
+    function setWidth() {
+      setCurrentWidth(window.innerWidth);
+    }
+    window.addEventListener('resize', setWidth);
+  });
+
   // eslint-disable-next-line no-unused-vars
   const [loading, error, vehicles] = useData();
+
+  // assign our arrays to variables
+  const vehicleList = vehicles[0];
+  const vehicleDetails = vehicles[1];
 
   if (loading) {
     return <div data-testid="loading">Loading</div>;
   }
 
   if (error) {
-    return <div data-testid="error">{ error }</div>;
+    return <div data-testid="error">{error}</div>;
   }
 
+  // extract relevant images from vehicle list
+  const extractImages = (id) => {
+    const relevantItem = vehicleList.filter((response) => response.id === id);
+    const mobileImage = relevantItem[0].media[1].url;
+    const largeImage = relevantItem[0].media[0].url;
+    return [mobileImage, largeImage];
+  };
+
+  // vehicle detail grid item
+  const vehicleGrid = vehicleDetails.map((res) => {
+    // get relevant image
+    const extractedImages = (extractImages(res.id));
+    // match image to screen size
+    const imageSource = currentWidth > 550 ? extractedImages[1] : extractedImages[0];
+    return (
+      <div className="vehicle-block" key={res.id}>
+        <img src={imageSource} alt="" />
+        <div className="vehicle-block__column">
+          <h2 className="vehicle-block__header"><span>{res.id}</span></h2>
+          <h3 className="vehicle-block__subheader">
+            From
+            <span> </span>
+            {res.price}
+          </h3>
+          <p className="vehicle-block__text">{res.description}</p>
+        </div>
+      </div>
+    );
+  });
+
   return (
-    <div data-testid="results">
-      <p>List of vehicles will be displayed here</p>
-      <p>
-        Visit
-        <a href="/api/vehicles.json" target="_blank"> /api/vehicles.json</a>
-        {' '}
-        (main endpoint)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_fpace.json" target="_blank">/api/vehicle_fpace.json</a>
-        {' '}
-        (detail endpoint - apiUrl)
-      </p>
-      <p>
-        Visit
-        <a href="/api/vehicle_xf.json" target="_blank">/api/vehicle_xf.json</a>
-        {' '}
-        (vehicle without any price)
-      </p>
+    <div data-testid="results" className="container">
+      <div className="VehicleList">
+        {vehicleGrid}
+      </div>
     </div>
   );
 }
